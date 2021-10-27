73c6ccaa1ff1ed5b8efa9cdf3d46cd952bd82476
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-10-26 22:04:44 -0500

    updated model index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,13 @@
+// Importing modularized seed files
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
+// Importing modularized seed files
 const sequelize = require('../config/connection');
 
+// Seed database with categories, products, tags, and productTags
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
@@ -23,4 +26,5 @@ const seedAll = async () => {
   process.exit(0);
 };
 
+//Start the seeding
 seedAll();
