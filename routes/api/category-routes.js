fa7d50d6e085aeb32cdf4d8a0299eaad37cab16b
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-10-27 12:57:58 -0500

    new category is working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,7 +4,6 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 //Get All Categories
 router.get('/', async (req, res) => {
-  // find all categories
   try {
     const categoryData = await Category.findAll({
       include: [{model:Product}],
@@ -15,8 +14,8 @@ router.get('/', async (req, res) => {
   }
 });
 
+//Get One category by its 'id' value
 router.get('/:id', async(req, res) => {
-  // find one category by its `id` value
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{model:Product}],
@@ -31,8 +30,14 @@ router.get('/:id', async(req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async(req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
